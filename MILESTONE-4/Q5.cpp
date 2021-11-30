dac79ff23a223916975276377f567a1bc6ac36aa
jahanvi18 <91457463+jahanvi18@users.noreply.github.com>
2021-11-30 20:13:48 +0530

    Add files via upload

---
+++ MILESTONE-4/Q5.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int size,i;
+    int arr[50];//variable declaration to size of array
+int large=0, secondLarge=0
+;
+
+cout<<"Enter the number of element in array: ";
+cin>>size;//taking input from user for no of elements in array
+
+ for(i=0; i<size; i++)
+ {
+    cout<<"Enter the array element"<<i+1<<": ";
+    cin>>arr[i];
+
+ if(large<arr[i])
+{
+secondLarge=large;
+large=arr[i];
+}
+else if(secondLarge<arr[i])
+{
+secondLarge=arr[i];
+ }
+}
+cout<<"\n";
+cout<<""<<large<<" is a largest nunber\n";
+cout<<secondLarge<<" is a second largest nunber";
+    return 0;
+}
